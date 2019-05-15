7c2dc6a3d88539cfc0318b13cba0ec91f97ad5f4
Daniele Praino <daniele.praino@gmail.com>
2019-05-15 22:52:10 +0200

    added template message

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+
+var now = new Date();
+var time = now.getHours() + ":" + now.getMinutes();
+
 $(".send-button").click(function(){
-  var newMessage = $(".input-message").val();
-  $(".main-chat").append("<p class='new-message usermsg'>" + newMessage + "</p>");
-  $(".main-chat").append("<p class='new-message friendmsg'>Messaggio di prova!</p>");
+  var newTextMsg = $(".input-message").val();
+  templateMsg = $(".template-message .new-message").clone();
+  templateMsg.children(".text-message").text(newTextMsg);
+  templateMsg.children(".info-message").children(".time-message").text(time);
+  templateMsg.addClass("usermsg");
+  $(".main-chat").append(templateMsg);
+  $(".input-message").val("");
+  autoRespond = setInterval(newFriendMessage, 1000);
 });
+
+function newFriendMessage() {
+  templateMsg = $(".template-message .new-message").clone();
+  templateMsg.children(".text-message").text("Ok!");
+  templateMsg.children(".info-message").children(".time-message").text(time);
+  templateMsg.children(".info-message").children("i").hide();
+  templateMsg.addClass("friendmsg");
+  $(".main-chat").append(templateMsg);
+  stopAutoRespond();
+}
+
+function stopAutoRespond(){
+  clearInterval(autoRespond);
+}
